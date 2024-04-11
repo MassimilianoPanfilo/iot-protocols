a6f076c2d2aa1e3ca94e30015938ef1e9c9f2e87
Massimiliano Panfilo <massimiliano.panfilo@stud.itsaltoadriatico.it>
2024-04-11 17:30:25 +0200

    Update mqtt server side

--- server/src/index.js
+++ server/src/index.js
@@ -1,26 +1,18 @@
 const express = require('express') //modulo framework express utile per il lavoro serverside, per gestire richieste http
 const helmet = require('helmet') //modulo per la sicurezza
-const mongoose = require('mongoose');
+
 
 const port = 8011
 const app = express() //Istanza del server
-const mongoURI = 'mongodb://localhost:27017/db_IoT_Protocols';// URL di connessione al database MongoDB
+
 
 app.use(helmet())
 app.use(express.json())
 
-// Connessione al database
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connessione al database MongoDB avvenuta con successo'))
-    .catch(err => console.error('Errore durante la connessione al database', err));
-/*
-* Continua
-*/
 
-    //Quì metto le rotte
+//Quì metto le rotte
 // Rotta per il metodo GET
 app.get('/', (req, res) => {
-    // Qui puoi gestire la logica per elaborare la richiesta GET
     res.send('Risposta per la richiesta GET');
 });
 
@@ -29,9 +21,6 @@ app.post('/', (req, res) => {
     console.log('Dati ricevuti:', req.body);
     res.json({ message: 'Dati ricevuti correttamente', data: req.body });
 });
-
-
-
 
 // avvio il server
 app.listen(port, () => {
